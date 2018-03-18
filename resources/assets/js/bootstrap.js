244f62c6d3f05ae8b802a3cfa9ab3900e868a220
Tony Messias <tonyzrp@gmail.com>
2018-03-17 23:51:28 -0300

    Remove jquery

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,7 +1,3 @@
-try {
-    window.$ = window.jQuery = require('jquery');
-} catch (e) {}
-
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
