9833a5849dc36d5b22c6fa2db56503545909e45a
Tony Messias <tonyzrp@gmail.com>
2018-04-29 20:37:29 -0300

    Bring back to single redis config

--- config/database.php
+++ config/database.php
@@ -108,18 +108,11 @@ return [
 
         'client' => 'predis',
 
-        'options' => [
-            'cluster' => 'redis',
-        ],
-
-
-        'clusters' => [
-            'default' => [
-                'host' => env('REDIS_HOST', '127.0.0.1'),
-                'password' => env('REDIS_PASSWORD', null),
-                'port' => env('REDIS_PORT', 6379),
-                'database' => 0,
-            ],
+        'default' => [
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port' => env('REDIS_PORT', 6379),
+            'database' => 0,
         ],
 
     ],
