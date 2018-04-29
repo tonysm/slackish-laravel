700012d2280f018d2c990013083552ab8fd3e478
Tony Messias <tonyzrp@gmail.com>
2018-04-29 20:09:30 -0300

    Configure redis for clustering

--- config/database.php
+++ config/database.php
@@ -108,11 +108,18 @@ return [
 
         'client' => 'predis',
 
-        'default' => [
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', 6379),
-            'database' => 0,
+        'options' => [
+            'cluster' => 'redis',
+        ],
+
+
+        'clusters' => [
+            'default' => [
+                'host' => env('REDIS_HOST', '127.0.0.1'),
+                'password' => env('REDIS_PASSWORD', null),
+                'port' => env('REDIS_PORT', 6379),
+                'database' => 0,
+            ],
         ],
 
     ],
