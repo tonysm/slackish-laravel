dac12232a68c769cc21fe4fdb532a5d44c7d4f47
Tony Messias <tonyzrp@gmail.com>
2018-04-29 22:52:57 -0300

    Run horizon metrics every minute

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('horizon:snapshot')->everyFiveMinutes();
+        $schedule->command('horizon:snapshot')->everyMinute();
     }
 
     /**
