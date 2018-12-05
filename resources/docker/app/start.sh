88134228738c6d5b63be7d2d6c7924cdf998b3c8
Tony Messias <tonyzrp@gmail.com>
2018-12-04 21:20:06 -0300

    Fix the start script

--- resources/docker/app/start.sh
+++ resources/docker/app/start.sh
@@ -5,25 +5,25 @@ set -e
 role=${CONTAINER_ROLE:-app}
 env=${APP_ENV:-production}
 
-if [ "$env" != "local" ]; then
+if [[ "$env" != "local" ]]; then
     echo "Caching configuration..."
     (cd /var/www/html && php artisan config:cache && php artisan route:cache && php artisan view:cache)
     echo "Running the migrations..."
     php /var/www/html/artisan migrate -f
 fi
 
-if [ "$role" = "app" ]; then
+if [[ "$role" = "app" ]]; then
 
     exec apache2-foreground
 
-elif [ "$role" = "queue" ]; then
+elif [[ "$role" = "queue" ]]; then
 
     echo "Running the queue..."
     php /var/www/html/artisan horizon
 
-elif [ "$role" = "scheduler" ]; then
+elif [[ "$role" = "scheduler" ]]; then
 
-    while [ true ]
+    while [[ true ]]
     do
         php /var/www/html/artisan schedule:run --verbose --no-interaction &
         sleep 60
